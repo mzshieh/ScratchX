933b5b905fc06a0ced7fff59f5b606e0ef2b6ab0
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2016-07-16 03:29:55 +0000

    update

--- extension.js
+++ extension.js
@@ -38,22 +38,28 @@
         window.speechSynthesis.speak(msg);
     };
 
-    ext.say_korean = function(text) {
-        if('ko-KR' in voice) {
-            /* global SpeechSynthesisUtterance */
-            var msg = new SpeechSynthesisUtterance(text);
-            msg.voice = voice['ko-KR'];
-            window.speechSynthesis.speak(msg);
-        }
-    };
-    
-    ext.say_lang = function(text,lang) {
+    ext.say_lang_rate_pitch_vol = function(text,lang,rate,pitch,vol) {
         if(lang in voice) {
             /* global SpeechSynthesisUtterance */
             var msg = new SpeechSynthesisUtterance(text);
             msg.voice = voice[lang];
+            msg.rate = rate;
+            msg.pitch = pitch;
+            msg.volume = vol;
             window.speechSynthesis.speak(msg);
         }
+    };
+
+    ext.say_lang_rate = function(text,lang,rate) {
+        ext.say_lang_rate_pitch_vol(text,lang,rate,1.0,1.0);
+    };
+
+    ext.say_lang = function(text,lang) {
+        ext.say_lang_rate(text,lang,1.0);
+    };
+
+    ext.say_zh = function(text) {
+        ext.say_lang_rate(text,'zh-TW',1.0);
     };
     
     // Block and block menu descriptions
@@ -61,8 +67,10 @@
         blocks: [
             // Block type, block name, function name
             [' ', 'say %s', 'say', "Hello!"],
-            [' ', 'say %s in 한국의', 'say_korean', '한국의'],
-            [' ', 'say %s in lang %s', 'say_lang', '說中文', 'zh-TW'],
+            [' ', '說%s', 'say_zh', '中文'],
+            [' ', 'say %s in lang %s', 'say_lang', '程式設計', 'zh-TW'],
+            [' ', 'say %s in lang %s at rate %n', 'say_lang_rate', '程式設計', 'zh-TW', 1],
+            [' ', 'say %s in lang %s at rate %n at pitch %n of volume %n', 'say_lang_rate_pitch_vol', '程式設計', 'zh-TW', 1, 1, 1],
         ],
     };
 
