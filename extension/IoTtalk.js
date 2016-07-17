a02d9e96535335cc26ca48ff172377c53abcff17
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2016-07-17 07:32:03 +0000

    Add a hat block

--- extension/IoTtalk.js
+++ extension/IoTtalk.js
@@ -86,6 +86,7 @@
         var new_query_timestamp = new Date().getTime();
         if(new_query_timestamp-last_query_timestamp>=flood_threshold) {
             ext.iottalk_remote_get(feature,function(){});
+            return false;
         }
         
         return false;
